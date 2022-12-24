cfe2f27a4bff0ae9957a7b0c8e097e46302696fa
Tayka <tayka@MacBook-Pro-Tayka.local>
2022-12-25 02:32:02 +0300

    Formate date improve

--- src/lib/utils/formatDate.ts
+++ src/lib/utils/formatDate.ts
@@ -2,6 +2,13 @@ import type { TournamentStatus } from "@prisma/client";
 
 // {tournamentInfo.startDate.toLocaleString("ru-RU", {  month: 'long', day: 'numeric'})} {tournamentInfo.startDate.toLocaleString("ru-RU", {  timeStyle:"short"})}
 
+const options = {
+  month: "short",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  hour12: false,
+};
 export function formatDate(
   date: number,
   now: number,
@@ -14,7 +21,12 @@ export function formatDate(
   const HOUR = MINUTE * 60;
   const DAY = HOUR * 24;
   if (diff < 0) {
-    if (tournamentStatus == "finished") return "Турнир завершен";
+    if (tournamentStatus == "finished") return "Завершен";
+    // + new Intl.DateTimeFormat("ru-RU", options).format(date)
+    // new Date(date).toLocaleString("ru-RU", {
+    //   dateStyle: "short",
+    //   timeStyle: "short",
+    // })
     if (tournamentStatus == "running") {
       const minutes = Math.floor(-diff / MINUTE) % 60;
       const seconds = Math.floor(-diff / SECOND) % 60;
@@ -22,13 +34,20 @@ export function formatDate(
 
       return `Идет ${minutes}:${secondString}`;
     }
-  } else if (diff < HOUR) {
+  }
+  if (tournamentStatus == "registration") if (diff < 0) return "Отменен";
+  if (diff < HOUR) {
     const minutes = Math.floor(diff / MINUTE) % 60;
     const seconds = Math.floor(diff / SECOND) % 60;
     const secondString = `${seconds < 10 ? "0" + seconds : seconds}`;
     return `Через ${minutes}:${secondString}`;
-  } else if (diff < DAY)
-    return `Через ${Math.round(diff / HOUR)} часов ${date.toLocaleString()}`;
+  } else if (diff < DAY) {
+    const minutes = Math.floor(diff / MINUTE) % 60;
+    const hours = Math.round(diff / HOUR);
+    const seconds = Math.floor(diff / SECOND) % 60;
+    const secondString = `${seconds < 10 ? "0" + seconds : seconds}`;
+    return `Через ${hours}:${minutes}:${secondString}`;
+  }
 
   return `${new Date(date).toLocaleString()}`;
 }
